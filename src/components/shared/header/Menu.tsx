3e04adff6f6eb98b25dbc51d8cedd15458f78c6e
Devmdmainuddin <devmdmainuddin@gmail.com>
2025-01-17 00:00:58 +0600

    add to card function add

--- src/components/shared/header/Menu.tsx
+++ src/components/shared/header/Menu.tsx
@@ -1,5 +1,6 @@
-import { ShoppingCartIcon } from 'lucide-react'
+// import { ShoppingCartIcon } from 'lucide-react'
 import Link from 'next/link'
+import CartButton from './cart-button'
 const Menu = () => {
   return (
     <div className='flex justify-end'>
@@ -12,11 +13,16 @@ const Menu = () => {
        Hello, Sign in
        
       </Link>
+      <div className='flex justify-end'>
+      <nav className='flex gap-3 w-full'>
+        <CartButton />
+      </nav>
+    </div>
 
-      <Link href='/cart' className='header-button'>
+      {/* <Link href='/cart' className='header-button'>
         <ShoppingCartIcon className='h-8 w-8' />
         <span className='font-bold'>Cart</span>
-      </Link>
+      </Link> */}
     </nav>
   </div>
   )
